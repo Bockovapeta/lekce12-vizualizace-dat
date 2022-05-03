4da64f590568eaa86450783c299f9e42eb36ac50
Petra Bočková <Wickky@seznam.cz>
2022-05-03 19:43:04 +0200

    add feature with other Nakazeni65

--- covid19-zakladni-info.js
+++ covid19-zakladni-info.js
@@ -2,43 +2,61 @@ fetch('./variables.json')
     .then(response => response.json())
     .then(variables => {
         fetch(`https://onemocneni-aktualne.mzcr.cz/api/v3/zakladni-prehled?apiToken=${variables.apiToken}`)
-        .then(response => response.json())
-        .then(data => {
-            // preulozeni dat do promenne a vypsani do konzole
-            let zakladniInfo = data['hydra:member'][0];
-            console.log(zakladniInfo);
+            .then(response => response.json())
+            .then(data => {
+                // preulozeni dat do promenne a vypsani do konzole
+                let zakladniInfo = data['hydra:member'][0];
+                console.log(zakladniInfo);
 
-            //datum upravy
-            let datumUpravy = new Date(zakladniInfo.datum);
-            document.getElementById('js-modifikovano').innerText = datumUpravy.toLocaleDateString('cs-CZ');
+                //datum upravy
+                let datumUpravy = new Date(zakladniInfo.datum);
+                document.getElementById('js-modifikovano').innerText = datumUpravy.toLocaleDateString('cs-CZ');
 
-            //aktualne hospitalizovani
-            document.getElementById('js-hospitalizovani').innerText = parseInt(zakladniInfo.aktualne_hospitalizovani).toLocaleString('cs-CZ');
+                //aktualne hospitalizovani
+                document.getElementById('js-hospitalizovani').innerText = parseInt(zakladniInfo.aktualne_hospitalizovani).toLocaleString('cs-CZ');
 
-            //pribytek za predchozi mereny den
-            document.getElementById('js-potvrzeni').innerText = parseInt(zakladniInfo.potvrzene_pripady_vcerejsi_den).toLocaleString('cs-CZ')
-            let datumPotvrzenych = new Date(zakladniInfo.potvrzene_pripady_vcerejsi_den_datum);
-            document.getElementById('js-potvrzeni-datum').innerText = datumPotvrzenych.toLocaleDateString('cs-CZ');
+                //pribytek za predchozi mereny den
+                document.getElementById('js-potvrzeni').innerText = parseInt(zakladniInfo.potvrzene_pripady_vcerejsi_den).toLocaleString('cs-CZ')
+                let datumPotvrzenych = new Date(zakladniInfo.potvrzene_pripady_vcerejsi_den_datum);
+                document.getElementById('js-potvrzeni-datum').innerText = datumPotvrzenych.toLocaleDateString('cs-CZ');
 
-            //ockovani za predchozi mereny den
-            document.getElementById('js-ockovani').innerText = parseInt(zakladniInfo.ockovane_osoby_vcerejsi_den).toLocaleString('cs-CZ')
-            let datumOckovanych = new Date(zakladniInfo.ockovane_osoby_vcerejsi_den_datum);
-            document.getElementById('js-ockovani-datum').innerText = datumOckovanych.toLocaleDateString('cs-CZ');
-        
-            // UKOL NA CVICENI
-            // pridej do prehledu podil pribytku nakazenych ve vekove 
-            // skupine 65+ za posledni mereny den z celkoveho pribytku
-            // nakazenych za posledni mereny den
+                //ockovani za predchozi mereny den
+                document.getElementById('js-ockovani').innerText = parseInt(zakladniInfo.ockovane_osoby_vcerejsi_den).toLocaleString('cs-CZ')
+                let datumOckovanych = new Date(zakladniInfo.ockovane_osoby_vcerejsi_den_datum);
+                document.getElementById('js-ockovani-datum').innerText = datumOckovanych.toLocaleDateString('cs-CZ');
 
-            // UKOL NA CVICENI
-            // proved validaci, zda plati podminka 
-            // celkem_potvrzeni = aktivni + vyleceni + umrti
-            // vysledek validace vypis do konzole pomoci console.log()
+                let podil = ((zakladniInfo.potvrzene_pripady_65_vcerejsi_den * 100) / zakladniInfo.potvrzene_pripady_vcerejsi_den)
+                console.log(podil);
+                document.getElementById('js-nakazeni65').innerText = (Math.round(podil * 100) / 100) + " %"
+                let datumNakazenych65 = new Date(zakladniInfo.potvrzene_pripady_65_vcerejsi_den_datum);
+                document.getElementById('js-nakazeni65-datum').innerText = datumNakazenych65.toLocaleDateString('cs-CZ');
 
-            // BONUSOVY UKOL NA CVICENI
-            // proved refactoring lokalizace datumu a cislnych hodnot
-            // tak, ze vyvoris dve separatni funkce pro lokalizaci
-        })
+
+
+                let potvrzenePripadyCelkem = zakladniInfo.potvrzene_pripady_celkem
+                let celkemPotvrzeni = zakladniInfo.aktivni_pripady + zakladniInfo.vyleceni + zakladniInfo.umrti
+
+
+
+                if (potvrzenePripadyCelkem === celkemPotvrzeni) {
+                    console.log("Potvrzené případy celkem se rovnají součtu aktivních nemocných, vyléčených a zemřelých");
+                }
+
+
+                // UKOL NA CVICENI
+                // pridej do prehledu podil pribytku nakazenych ve vekove 
+                // skupine 65+ za posledni mereny den z celkoveho pribytku
+                // nakazenych za posledni mereny den
+
+                // UKOL NA CVICENI
+                // proved validaci, zda plati podminka 
+                // celkem_potvrzeni = aktivni + vyleceni + umrti
+                // vysledek validace vypis do konzole pomoci console.log()
+
+                // BONUSOVY UKOL NA CVICENI
+                // proved refactoring lokalizace datumu a cislnych hodnot
+                // tak, ze vyvoris dve separatni funkce pro lokalizaci
+            })
     })
     .catch(error => {
         console.error('Chyba aplikace (zakladni info): ', error);
